3eb15b811f0e151f1619471ebd76dd8e6ee53d7f
DNA-RNA <77885953+DNA-RNA@users.noreply.github.com>
2021-03-06 14:54:19 +0300

    ReCapProject

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            Console.WriteLine("Araç kiralama sistemimize hoşgeldiniz \n");
+            Console.WriteLine("<<<<<<<<<< Araçlarımız >>>>>>>>>> \n");
+           
+            foreach (var car in carManager.GetAll())
+            {
+                
+                Console.WriteLine("<<<"+car.Description + ">>>");
+                Console.WriteLine();
+            }
+            //CarId=1-> binek, 2-> ticari, 3-> servis || BrandId=1-> Bmw, 2-> Mercedes, 3-> Volkswagen, 4-> Renault
+            //ColorId=1-> kırmızı, 2-> siyah, 3-> mavi, 4-> beyaz renkleri temsil ediyor
+            
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId=1,BrandId=1,ColorId=1,ModelYear=2018,DailyPrice=245,Description="Binek BMW M3 kırmızı 2018 model günlüğü 245 tl"},
+                new Car{CarId=1,BrandId=3,ColorId=3,ModelYear=2020,DailyPrice=245,Description="Binek Volkswagen Tiguan mavi 2020 model günlüğü 245 tl"},
+                new Car{CarId=1,BrandId=2,ColorId=4,ModelYear=2019,DailyPrice=245,Description="Binek Mercedes A 180 AMG beyaz 2019 model günlüğü 245 tl"},
+                new Car{CarId=2,BrandId=4,ColorId=1,ModelYear=2019,DailyPrice=318,Description="Ticari Renault Kangoo kırmızı 2019 model günlüğü 318 tl"},
+                new Car{CarId=2,BrandId=3,ColorId=4,ModelYear=2019,DailyPrice=318,Description="Ticari Volkswagen Caddy beyaz 2019 model günlüğü 318 tl"},
+                new Car{CarId=3,BrandId=2,ColorId=2,ModelYear=2020,DailyPrice=350,Description="Servis Mercedes Sprinter siyah 2020 model günlüğü 350 tl"},
+                new Car{CarId=3,BrandId=3,ColorId=2,ModelYear=2021,DailyPrice=350,Description="Servis Volkswagen  Transporter siyah 2021 model günlüğü 350 tl"},
+            };
+        }
+        
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId && c.BrandId == car.BrandId && c.ColorId == car.ColorId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int brandId, int colorId)
+        {
+            return _cars.Where(c => c.BrandId == brandId && c.ColorId == colorId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId && c.BrandId == car.BrandId && c.ColorId == car.ColorId);
+            carToUpdate.CarId = car.CarId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+
+        }
+    }
+}
